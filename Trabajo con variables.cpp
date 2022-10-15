f761d74c266575f505e76fba2502275dc07272f3
fernando <crosio.fernando@gmail.com>
2022-10-15 17:20:23 -0300

    variables en c++

---
+++ Trabajo con variables.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+   string nombre="Juan";
+
+   int edad {21};
+   double salario;
+   cout<<"Introduce el salario del empleado: ";
+   cin>> salario;
+   cout<<"Introduce la edad del empleado: ";
+   cin>> edad;
+   cout<<"Datos del empleado: " <<endl;
+   cout<<"Nombre: " <<nombre<<". Edad: "<<edad<<". Salario: "<<salario;
+    
+}
